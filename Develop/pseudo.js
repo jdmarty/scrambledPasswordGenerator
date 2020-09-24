f2d9d614c50d25583fe4d22901fc110d91a44cef
JD Marty <joshua.dmarty@gmail.com>
2020-09-23 19:12:40 -0700

    first commit with addition of pseudocode

---
+++ Develop/pseudo.js
@@ -0,0 +1,36 @@
+//assign four arrays to variables lowercaseLetters, uppercaseLetters, numbers, specialChar containing appropriate character type
+//assign empty array to variable characterOptions
+//assign empty string to variable password
+//assign four false booleans to variables includesLCL, includesUCL, includesNum, and includesSpec
+
+//Prompt user to enter a number of characters between 8 and 128
+    //while loop to validate response is a number in acceptable range
+
+//assign variable to Confirm to ask user if they want to include lowercase letters
+//assign variable to Confirm to ask user if they want to include uppercase letters
+//assign variable to Confirm to ask user if they want to include numbers
+//assign variable to Confirm to ask user if they want to include special characters
+//assign variable to Confirm that presents user with their selections and asks if they would like to continue
+    //exit function if they do not confirm
+    //alert no valid inputs if none of the options were confirmed and exit function
+
+//if confirmLowercase === true then
+    //concat lowercaseLetters array to characterOptions
+//if confirmUppercase === true then
+    //concat uppercaseLetters array to characterOptions
+//if confirmNumbers === true then
+    //concat numbers array to characterOptions
+//if confirmSpecial === true then
+    //concat specialChar to characterOptions
+
+//assign four false booleans to variables includesLCL, includesUCL, includesNum, and includesSpec
+//for loop to run while password.length is less than passwordLength prompt
+    //generate a random integer between 0 and the length of the character options array
+    //add the character at that random index to the password string
+        //check if the character that was just added is in lowercaseLetters, change includesLCL to true if it is
+        //check if the character that was just added is in uppercaseLetters, change includesUCL to true if it is
+        //check if the character that was just added is in numbers, change includesNum to true if it is
+        //check if the character that was just added is in specialChar, change includesSpec to true if it is
+
+//outside the for loop, check that all the includes variables match the confirm values. Run the loop again if they do not
+//return password
